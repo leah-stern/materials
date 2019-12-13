9d855aef78741996edba4b9ff21adb9bcb582509
leah-stern <leah.stern@tufts.edu>
2019-12-12 23:54:05 -0500

    everythings clickable now

--- database.py
+++ database.py
@@ -1,24 +1,41 @@
 from flask import Flask
 from flask_table import Table, Col
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# from flask_script import Manager
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, BooleanField
 from wtforms.validators import DataRequired
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 import os
+from flask_cors import CORS
 
 # set up sqlalchemy app & database
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SECRET_KEY'] = '41e2419b1b446d6eb75f46ee078cbf54'
+app.config['CORS_HEADERS'] = 'Content-Type'
+os.environ['DATABASE_URL'] = 'postgresql://postgres:postgres@postgres/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+migrate = Migrate(app, db) 
+# manager = Manager(app)
+
+# manager.add_command('db', MigrateCommand)
+
+# association table to handle many-to-many relationship between chemicals and formulations
+association = db.Table('association',
+	db.Column('formulation_id', db.Integer, db.ForeignKey('formulation.formula_id'), primary_key=True),
+	db.Column('chemical_id', db.Integer, db.ForeignKey('chemical.id'), primary_key=True)
+)
 
 #TODO: what about the pdfs?? need a way to upload files
 #TODO: and the structure PNG! should that be generated automatically or uploaded?
 #TODO: click GHS hazard icons for each chemical
 class Chemical(db.Model):
 	id = db.Column(db.Integer, primary_key=True) # TODO: this should be the CAS number!!
+	formulas= db.relationship('Formulation', secondary=association, lazy='dynamic')
 	name = db.Column(db.String(200), nullable=True)
 	alternate_name = db.Column(db.String(200), nullable=True)
 	vendor = db.Column(db.String(200), nullable=True)
@@ -68,28 +85,66 @@ class Chemical(db.Model):
 class Project(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(200), nullable=False)
+	project_lead = db.Column(db.String(200), nullable=True)
+	goal = db.Column(db.String(200), nullable=True)
+	date_started = db.Column(db.String(200), nullable=True) #TODO: what format? how can we force the format?
+	formulations = db.relationship('Formulation', backref='id', lazy='dynamic')
 	# sheet_URL = db.Column(db.String(200), nullable=True)
-	# date_started = db.Column(db.String(200), nullable=True) #TODO: what format? how can we force the format?
 	# business_unit = db.Column(db.String(200), nullable=True)
-	# lead_experimenter = db.Column(db.String(200), nullable=True)
-	# lead_PM = db.Column(db.String(200), nullable=True)
 	# other_members = db.Column(db.String(200), nullable=True) #TODO: this should be a list
 	# product_contact = db.Column(db.String(200), nullable=True)
-	# goal = db.Column(db.String(200), nullable=True)
 	# prior_data = db.Column(db.String(200), nullable=True) #TODO: what is this?
 	# notable_formulations = db.Column(db.String(200), nullable=True) #TODO: what is this?
+
+class Formulation(db.Model):
+	formula_id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+	chemicals = db.relationship('Chemical', secondary='association', lazy='dynamic')
+	# chemical_amounts = db.relationship('Amounts', backref='formula_id', lazy='dynamic')
+	results = db.relationship('Results', uselist=False, backref='formula_id')
+
+	def __repr__(self):
+		return self.name
+
+# class Amounts(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	chemical_id = db.Column(db.Integer, nullable=False)
+# 	value = db.Column(db.Integer, nullable=True)
+# 	formulation = db.Column(db.Integer, db.ForeignKey('formulation.formula_id'), nullable=False)
+# 	# TODO: add weight in grams
+
+class Results(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	thoughts = db.Column(db.String(200), nullable=True)
+	A_MA = db.Column(db.String(200), nullable=True)
+	composition_notes = db.Column(db.String(200), nullable=True)
+	filler_vol_percent = db.Column(db.Integer, nullable=True)
+	filler_weight_percent = db.Column(db.Integer, nullable=True)
+	formulation = db.Column(db.Integer, db.ForeignKey('formulation.formula_id'), nullable=False)
+	# dp = db.Column(db.Integer, nullable=True)
+	# ec = db.Column(db.Integer, nullable=True)
+	# UTS = db.Column(db.Integer, nullable=True)
+	# tensile_modulus = db.Column(db.Integer, nullable=True)
+	# strain_at_break = db.Column(db.Integer, nullable=True)
+	# FS = db.Column(db.Integer, nullable=True)
+	# flexural_modulus = db.Column(db.Integer, nullable=True)
+	# flex_strain_at_break = db.Column(db.Integer, nullable=True)
+	# viscosity_25C = db.Column(db.Integer, nullable=True)
+	# viscosity_35C = db.Column(db.Integer, nullable=True)
+	# tg = db.Column(db.Integer, nullable=True)
+	# HDT = db.Column(db.Integer, nullable=True)
 
 # Form for adding new projects to the database
 class addProject(FlaskForm):
 	id = db.Column(db.Integer, primary_key=True)
 	name = StringField('')
-	# url = StringField('')
 
 # Table for displaying all projects in database
 class displayFormulas(Table):
 	id = Col('id', show=False)
 	name = Col('Name')
-	# url = Col('sheet_URL')
+	project_lead = Col('Project Lead')
 
 # Table for displaying all chemicals in database
 class displayChemicals(Table):
@@ -111,4 +166,6 @@ class searchForm(FlaskForm):
 	id = db.Column(db.Integer, primary_key=True)
 	search = StringField('')
 
+# if __name__ == '__main__':
+# 	manager.run()
 
--- main.py
+++ main.py
@@ -5,32 +5,39 @@ import flask
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
-from httplib2 import Http
-import oauth2client
-from oauth2client import file, client, tools
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from database import db, app, Chemical, addChemical, displayChemicals, searchForm, Project, addProject, displayFormulas
-from flask import render_template, request, redirect
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from oauth2client.service_account import ServiceAccountCredentials
-import gspread
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from httplib2 import Http
+from database import db, app, Chemical, addChemical, displayChemicals, searchForm, Project, addProject, displayFormulas, Formulation, Results
+from flask import render_template, request, redirect
 import os
+from sqlalchemy import func
+from flask import jsonify
+from flask_cors import cross_origin
+
+# import oauth2client
+# from oauth2client import file, client, tools
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+# from wtforms.ext.sqlalchemy.fields import QuerySelectField
+# import gspread
 
 # Scopes for Google Drive and Google Sheets APIs
 SCOPES = ['https://www.googleapis.com/auth/drive',
       'https://www.googleapis.com/auth/spreadsheets']
 
-CLIENT_SECRETS_FILE = "client_id.json"
+CLIENT_SECRETS_FILE = "client_secret.json"
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+API_SERVICE_NAME = 'drive'
+API_VERSION = 'v3'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # create database
-    # db.drop_all()
     db.create_all()
+
     return render_template("home.html")
 
 @app.route('/chemicals', methods=['GET', 'POST'])
@@ -41,15 +48,18 @@ def chemical_inventory():
     # form for searching chemicals in database
     search = searchForm(request.form)
 
-    if request.method == 'POST':
-        print('created', flush=True)
-        if request.form['button'] == 'Add':
-            create_chemical(form)
-        else: 
-            return search_chemical(search)
-
     # get all names for autofill search
-    tags = [chemical.name for chemical in Chemical.query.all()]
+    tags = jsonify([chemical.name for chemical in Chemical.query.all()])
+
+    if request.method == 'POST':
+        if request.form['button'] == 'Add':
+            created = create_chemical(form)
+            print(created, flush=True)
+        else: 
+            selection = search.data['search']
+            if selection != '':
+                results = Chemical.query.filter(Chemical.name.ilike('%' + str(selection) + '%'))
+                return render_template("chemical_db.html", form=form, allTags=tags, searchForm=search, chemicals=results)
 
     # # get all chemicals to display in table format, order by name
     all_chemicals = Chemical.query.order_by(Chemical.name).all()
@@ -91,20 +101,16 @@ def create_chemical(form):
         db.session.add(newChemical)
         db.session.commit()
 
-# Search chemicals by name
-def search_chemical(form):
-    selection = form.data['search']
+# TODO: only one details page, select chemical or proj details based on query parameter
+@app.route('/chemical_details', methods=['GET', 'POST'])
+def chemical_details():
+    # get chemical id from query parameter
+    chemical_id = request.args.get('id', default = -1, type=int)
 
-    results = Chemical.query.filter(Chemical.name==str(selection))
+    # get chemical with the current chemical ID
+    currentChemical = Chemical.query.filter(Chemical.id==chemical_id).first()
 
-    result_table = displayChemicals(results)
-    result_table.border = True
-
-    return render_template('results.html', table=result_table)
-
-@app.route('/more_info', methods=['GET', 'POST'])
-def more_info():
-    return render_template("more_info.html")
+    return render_template("chemical_details.html", selected=currentChemical)
 
 # TODO: this could be a lot more elegant, a loop or switch statement would probably be better
 def assign_GHS_values(form, newChemical):
@@ -140,87 +146,215 @@ def formulation_database():
     # create form for adding new project to database
     form = addProject(request.form)
 
+    # form for searching projects in database
+    search = searchForm(request.form)
+
     # validate form
     if request.method == 'POST':
-        newProject = Project(
-            name=request.form.get("Name")
-        )
+        if request.form['button'] == 'Add':
+            newProject = Project(
+                name=request.form.get("Name"),
+                project_lead=request.form.get("Project Lead"),
+                goal=request.form.get("Goal"),
+                date_started=request.form.get("Date Started"),
+                formulations = {}
+            )
 
-        # add and commit new chemical
-        db.session.add(newProject)
-        db.session.commit()
+            newProject.formulations.chemicals = {}
 
-        # create_sheet(newProject.name)
-        print('created')
+            populate_formulations(newProject)
+
+            # add and commit new chemical
+            db.session.add(newProject)
+            db.session.commit()
+
+            # create_sheet(newProject.name)
+
+        else:
+            selection = search.data['search']
+            if selection != '':
+                results = Project.query.join(Formulation, Project.formulations)\
+                                .filter( (Project.name.ilike('%' + str(selection) + '%')) |\
+                                (Formulation.name.ilike('%' + str(selection) + '%')))
+
+                return render_template("formulation_db.html", form=form, projects=results, searchForm=search)
 
     # get all projects to display in table format
     all_projects = Project.query.order_by(Project.name).all()
 
     # render the form and all projects
-    return render_template("formulation_db.html", form=form, projects=all_projects)
+    return render_template("formulation_db.html", form=form, projects=all_projects, searchForm=search)
 
-@app.route('/create_project_sheet', methods=['GET', 'POST'])
+# TODO: loop through spreadsheet data once i get that working
+def populate_formulations(project):
+    # TODO: remove, this is just for testing, read from sheet to create
+    # could appending all this be a function defined within the formulation class?
+    everybody = Chemical.query.all()
+    one = Chemical.query.first()
+
+    sample_results_1 = Results(
+        thoughts = 'just a thought',
+        A_MA = 'A/MA',
+        composition_notes = 'none',
+        filler_vol_percent = 23,
+        filler_weight_percent = 31
+    )
+
+    sample_results_2 = Results(
+        thoughts = 'another thought',
+        A_MA = 'A',
+        composition_notes = 'none',
+        filler_vol_percent = 27,
+        filler_weight_percent = 30
+    )
+
+    newFormula1 = Formulation(name='H30f0')
+    for chemical in everybody:
+        newFormula1.chemicals.append(chemical)
+    # newFormula1.results.append(sample_results_1)
+    project.formulations.append(newFormula1)
+
+    newFormula2 = Formulation(name='HFm006_f55')
+    newFormula2.chemicals.append(one)
+    # newFormula2.results.append(sample_results_2)
+    project.formulations.append(newFormula2)
+
+@app.route('/project_details', methods=['GET', 'POST'])
+def project_details():
+    # get project id from query parameter
+    project_id = request.args.get('id', default = -1, type=int)
+
+    # get project with the current project ID
+    currentProject = Project.query.filter(Project.id==project_id).first()
+
+    # pass the current project to the project_details page
+    return render_template("project_details.html", selected=currentProject)
+
+@app.route('/authenticate', methods=['GET', 'POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def authenticate():
+    print("here!", flush=True)
+    # get token id from POST
+    token_id = request.args.get('idtoken', default = -1, type=int)
+
+    print(token_id, flush=True)
+
+    return render_template("home.html")
+
+    # if 'credentials' not in flask.session:
+    #     return flask.redirect('authorize')
+
+    # # Load credentials from the session.
+    # credentials = google.oauth2.credentials.Credentials(**flask.session['credentials'])
+    # print("Credentials: ", credentials)
+
+    # drive = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    # files = drive.files().list().execute()
+
+    # # Save credentials back to session in case access token was refreshed.
+    # # ACTION ITEM: In a production app, you likely want to save these
+    # #              credentials in a persistent database instead.
+    # flask.session['credentials'] = credentials_to_dict(credentials)
+
+    # return flask.redirect(flask.url_for('home'))
+
+@app.route('/authorize')
+def authorize():
+    # Use the client_secret.json file to identify the application requesting
+    # authorization. The client ID (from that file) and access scopes are required.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+
+    # Indicate where the API server will redirect the user after the user completes
+    # the authorization flow. The redirect URI is required. The value must exactly
+    # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
+    # configured in the API Console. If this value doesn't match an authorized URI,
+    # you will get a 'redirect_uri_mismatch' error.
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    # Generate URL for request to Google's OAuth 2.0 server.
+    # Use kwargs to set optional request parameters.
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+
+    flask.session['state'] = state
+
+    return flask.redirect(authorization_url)
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verified in the authorization server response.
+  state = flask.session['state']
+
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = flask.request.url
+  print("Auth response: ", authorization_response)
+  flow.fetch_token(authorization_response=authorization_response)
+
+  # Store credentials in the session.
+  # ACTION ITEM: In a production app, you likely want to save these
+  #              credentials in a persistent database instead.
+  credentials = flow.credentials
+  flask.session['credentials'] = credentials_to_dict(credentials)
+
+  return flask.redirect(flask.url_for('authenticate'))
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
 def create_sheet(projectName):
-    # creds = None
-    # # The file token.pickle stores the user's access and refresh tokens, and is
-    # # created automatically when the authorization flow completes for the first
-    # # time.
-    # if not creds or not creds.valid:
-    #     print("here2!", flush=True)
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         print("here3!", flush=True)
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             'credentials.json', SCOPES)
-    #         creds = flow.run_flow(port=8080)
-    #         print("here4!", flush=True)
-    #     # Save the credentials for the next run
-    #     print("here5!", flush=True)
-    #     with open('token.pickle', 'wb') as token:
-    #         pickle.dump(creds, token)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', SCOPES)
 
-    store = oauth2client.file.Storage('storage.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-
-    print("here6!", flush=True)
     service = build('drive', 'v4', credentials=creds, http=creds.authorize(Http()))
     sheets = build('sheets', 'v4', credentials=creds)
 
     body = {
         'mimeType': 'application/vnd.google-apps.spreadsheet',
         'name': projectName,
+        'supportsAllDrives': True,
+        'driveId': '0ADcJ0eHZoAh4Uk9PVA',
     }
 
     file = service.files().create(body=body).execute()
     sheet_id = file.get('id')
 
-    # print(list)
+    # # print(list)
 
-    data = {'requests': [
-        {'setDataValidation': {
-        'range': {
-            'startRowIndex': 0,
-            'endRowIndex': 1,
-            'startColumnIndex': 0,
-            'endColumnIndex': 1
-        },
-        'rule': {
-            'condition': {
-                'type': 'ONE_OF_LIST',
-                    'values': [
-                        {'userEnteredValue': 'one'},
-                        {'userEnteredValue': 'two'},
-                        {'userEnteredValue': 'three'},
-                    ]
-            },
-            'strict': True
-        }}}]}
+    # data = {'requests': [
+    #     {'setDataValidation': {
+    #     'range': {
+    #         'startRowIndex': 0,
+    #         'endRowIndex': 1,
+    #         'startColumnIndex': 0,
+    #         'endColumnIndex': 1
+    #     },
+    #     'rule': {
+    #         'condition': {
+    #             'type': 'ONE_OF_LIST',
+    #                 'values': [
+    #                     {'userEnteredValue': 'one'},
+    #                     {'userEnteredValue': 'two'},
+    #                     {'userEnteredValue': 'three'},
+    #                 ]
+    #         },
+    #         'strict': True
+    #     }}}]}
 
-    update = sheets.spreadsheets().batchUpdate(spreadsheetId=sheet_id, body=data).execute()
+    # update = sheets.spreadsheets().batchUpdate(spreadsheetId=sheet_id, body=data).execute()
 
 if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     app.run(host='0.0.0.0')
